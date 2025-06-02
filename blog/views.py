d05261aa9daeded2a9cc1b291f80c1bb8529aff1
Zi Lin <kowzilin@gmail.com>
2025-06-02 21:03:52 +0800

    Add function: Create blog

--- blog/views.py
+++ blog/views.py
@@ -1,5 +1,12 @@
+from django.contrib.auth.decorators import login_required
 from django.http import HttpResponse
-from django.shortcuts import render
+from django.shortcuts import render, reverse
+from django.urls.base import reverse_lazy
+from django.views.decorators.http import require_http_methods
+from django.http.response import JsonResponse
+
+from .models import *
+from .forms import *
 
 # Create your views here.
 
@@ -11,7 +18,50 @@ def index(request):
 def blog_detail(request, blog_id):
     return render(request, "blog/blog_detail.html")
 
-
+@login_required(login_url=reverse_lazy("appauth:login"))
+@require_http_methods(["GET", "POST"])
 def create_blog(request):
-    return render(request, "blog/create_blog.html")
+
+    if request.method == "GET":
+        categories = BlogCategory.objects.all()
+
+        return render(request, "blog/create_blog.html", {
+            "categories": categories,
+        })
+
+    else:
+        form = CreateBlogForm(request.POST)
+
+        if form.is_valid():
+            title = form.cleaned_data.get("title")
+            content = form.cleaned_data.get("content")
+            category_id = form.cleaned_data.get("category")
+
+            category = BlogCategory.objects.get(id=category_id)
+
+            blog = Blog.objects.create(
+                title=title,
+                content=content,
+                category=category,
+                author=request.user
+            )
+
+            return JsonResponse({
+                "code": 200,
+                "message": "successfully created blog",
+                "data": {
+                    "blog_id": blog.id
+                }
+            }, status=200)
+
+        else:
+            print(form.errors)
+            return JsonResponse({
+                "code": 400,
+                "message": "form invalid",
+            }, status=400)
+
+
+
+
 
--- static/js/create_blog.js
+++ static/js/create_blog.js
@@ -1,17 +1,13 @@
 window.onload = function() {
 
-// Step 1: Access i18nChangeLanguage from wangEditor
-    const { createEditor, createToolbar, i18nChangeLanguage } = window.wangEditor
-
-    // Step 2: Set language to English BEFORE creating editor
-    i18nChangeLanguage('en')
+    const { createEditor, createToolbar } = window.wangEditor
 
     const editorConfig = {
         placeholder: 'Type here...',
         onChange(editor) {
             const html = editor.getHtml()
             console.log('editor content', html)
-// You can sync HTML to <textarea>
+            // You can sync HTML to <textarea>
         }
     }
 
@@ -29,6 +25,40 @@ window.onload = function() {
         selector: '#toolbar-container',
         config: toolbarConfig,
         mode: 'default', // or 'simple'
+    })
+
+
+
+    $("#submit-btn").click(function (event) {
+
+        event.preventDefault()
+
+        let title = $("input[name='title']").val()
+        let category = $("#form-category").val()
+        let content = editor.getHtml()
+        let csrfmiddlewaretoken = $("input[name='csrfmiddlewaretoken']").val()
+
+        $.ajax('/create', {
+            method:"POST",
+            data: {
+                title,
+                content,
+                category,
+                csrfmiddlewaretoken
+            },
+            success: function(result) {
+                if (result["code"] == 200) {
+                    let blog_id = result["data"]["blog_id"]
+                    window.location = "/blog/" + blog_id
+                }
+                else {
+                    alert(result["message"])
+                }
+            }
+        })
+
+
+
     })
 
 }
